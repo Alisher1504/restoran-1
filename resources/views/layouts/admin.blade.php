a0e2c86cd3c3909257018b6e21ea4dcb08e3eec5
Alisher1504 <ruzmetov150499@gmail.com>
2023-02-14 13:00:21 +0500

    admin template add successfully

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/favicon/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/favicon/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/favicon/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/favicon/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/favicon/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/favicon/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/favicon/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/favicon/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/favicon/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/favicon/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/favicon/manifest.json') }}">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('assets/simplebar/css/simplebar.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/simplebar/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/simplebar/css/example.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+
+        @include('layouts.admin-inc.sidebar')
+
+        <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+
+            @include('layouts.admin-inc.header')
+
+            <div class="body flex-grow-1 px-3">
+                <main class="py-4">
+                    @yield('content')
+                </main>
+            </div>
+
+            @include('layouts.admin-inc.footer')
+
+        </div>
+        
+
+        
+    </div>
+
+
+    <script src="{{ asset('assets/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/simplebar/js/chart.min.js') }}"></script>
+    <script src="{{ asset('assets/chartjs/js/coreui-chartjs.js') }}"></script>
+    <script src="{{ asset('assets/utils/js/coreui-utils.js') }}"></script>
+
+
+</body>
+</html>
