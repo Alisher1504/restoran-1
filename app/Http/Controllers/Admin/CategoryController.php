a8e5529f6d1cd07ecc8006ab1edfbf3aec81a4e7
Alisher1504 <ruzmetov150499@gmail.com>
2023-02-17 14:46:09 +0500

    table create all page

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
     
     public function index(){
-        return view('admin.Category.index');
+        $category = Category::all();
+        return view('admin.Category.index', compact('category'));
     }
 
 }
