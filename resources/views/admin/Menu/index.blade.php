ae3f4034200b098fd1f22518b1595c4487da4cce
Alisher1504 <ruzmetov150499@gmail.com>
2023-02-25 10:29:45 +0500

    view menu table successfully

--- resources/views/admin/Menu/index.blade.php
+++ resources/views/admin/Menu/index.blade.php
@@ -20,30 +20,26 @@
                                 <table class="table table-dark table-striped">
                                     <thead>
                                         <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Actions</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
+
+                                        @foreach ($menu as $item)
+                                            <tr>
+                                                <th scope="row">{{ $item->name }}</th>
+                                                <td>{{ $item->description }}</td>
+                                                <td> {{ $item->price }} </td>
+                                                <td>
+                                                    <a href="" class="btn btn-success">Edit</a>
+                                                    <a href="" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+
                                     </tbody>
                                 </table>
                             </div>
