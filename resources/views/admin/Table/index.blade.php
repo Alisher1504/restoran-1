61d553bca5b9b1a7b303c166307829c232782b71
Alisher1504 <ruzmetov150499@gmail.com>
2023-03-01 11:17:16 +0500

    t5able view page create successfully

--- resources/views/admin/Table/index.blade.php
+++ resources/views/admin/Table/index.blade.php
@@ -20,30 +20,26 @@
                                 <table class="table table-dark table-striped">
                                     <thead>
                                         <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Guest number</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Location</th>
+                                            <th scope="col">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
+                                        @foreach ($table as $item)
+                                            <tr>
+                                                <th >{{ $item->name }}</th>
+                                                <td>{{ $item->guest_number }}</td>
+                                                <td>{{ $item->status->name }}</td>
+                                                <td>{{ $item->location->name }}</td>
+                                                <td>
+                                                    <a href="" class="btn btn-success text-white">Edit</a>
+                                                    <a href="" class="btn btn-danger text-white">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
